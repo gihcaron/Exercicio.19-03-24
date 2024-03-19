c61fa84d6998911142a8bb3437a40b75d43e5dc3
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-19 10:35:38 -0300

    Exercício contador de vogais - Versão 2

--- ResoluçãoEProblema.js
+++ ResoluçãoEProblema.js
@@ -37,6 +37,7 @@ console.log(`Quais vogais tem: ${ordem.join(", ")}.`);
 
 console.log(`Número de vogais: ${vogais.length}.`);
 
-// Saída esperada: Texto: amo codar.
-//Quais vogais tem: a, o, o.
-//Número de vogais: 3.
+// Saída esperada: 
+//Texto: desenvolver de sistemas é a minha maior paixão.
+//Quais vogais tem: a, a, a, a, a, e, e, e, e, e, i, i, i, i, o, o, o.
+//Número de vogais: 17.
